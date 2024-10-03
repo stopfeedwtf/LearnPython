3ee425fd800704e184fcf536e22ba7f7a9ba4406
stopfeedwtf <leo.bozon2001@gmail.com>
2024-10-03 12:44:41 +0200

    second commit

---
+++ fourthchapter.py
@@ -0,0 +1,16 @@
+# Occurences(Done)
+# list = [1,2,3,8,52,2,2]
+
+# def occurences(list,element):
+#     count=0
+#     for i in range(len(list)):
+#         if list[i] == element:
+#             count += 1
+#     return count
+
+# print(occurences(list, 2))
+
+# Recherche
+list = [1,2,3,8,52,2,2]
+
+def search(list,element):
